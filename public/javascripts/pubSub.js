a4f0b1dec22d56987ff549d940c5f4c364cf65c8
Howard Stearns <howard.stearns@gmail.com>
2025-12-03 09:12:26 -0800

    clean up (re-)connection; kill recursive errors

--- public/javascripts/pubSub.js
+++ public/javascripts/pubSub.js
@@ -6,80 +6,57 @@ const WEBSOCKET_URI = location.origin.replace('^http', 'ws') + '/ws'; // Falsey 
 const RETRY_SECONDS = 90;
 const INACTIVITY_SECONDS = 5 * 60;
 
-const handlers = {}; // Mapping key => function(messageData) for all active subcriptions
-
-let connection, promise, countdown;
-export async function setupNetwork() { // Establish or re-establish a connection
+let connectionPromise = null, countdown;
+async function send(messageObject) { // Send serialized message when ready, or nothing if no connection.
+  (await connectionPromise)?.send(JSON.stringify(messageObject));
+}
+async function close(...rest) { // Close network connection, if any.
+  (await connectionPromise)?.close(...rest);
+}
+async function setupNetwork() { // Establish or re-establish a connection.
   clearInterval(countdown);
-  const existing = await connection;
-  if (existing?.readyState === WebSocket.OPEN) {
+  if ((await connectionPromise)?.readyState === WebSocket.OPEN) {
     console.log('already connected');
     return;
   }
-  connection = WEBSOCKET_URI ?
-    new WebSocket(WEBSOCKET_URI) :
-    { // If no WEBSOCKET_URI, operate locally with an object that has a send() method
-      send(string) {
-	const {method, ...data} = JSON.parse(string);
-	if (method !== 'publish') return;
-	this.onmessage( {data: JSON.stringify(data)} ); // Fake an Event object.
-      }
-    };
-
-  promise = new Promise(resolve => // Resolves when open, b/c sending over a still-opening socket gives error.
+  connectionPromise = new Promise(resolve => { // Resolves to connection when open, b/c sending over a still-opening socket gives error.
+    const connection = new WebSocket(WEBSOCKET_URI);
+    connection.onmessage = event => receive(JSON.parse(event.data));
     connection.onopen = () => {
       if (connection.readyState !== WebSocket.OPEN) return; // You would think that can't happen, but...
       console.log('connection open');
       resolve(connection);
-    });
-
-  // onerror is of no help, as the event is generic.
-  connection.onclose = event => {
-    console.warn('websocket close', event.code, event.wasClean, event.reason);
-    if (event.reason === 'inactivity') return;
-    if (document.visibilityState === 'visible') {
-      let counter = RETRY_SECONDS;
-      countdown = setInterval(() => {
-	if (document.visibilityState !== 'visible') {
-	  console.log("Abandoning retry timeout for invisible tab.");
-	  clearInterval(countdown);
-	  return;
-	}
-	if (counter > 1) {
-	  showMessage(Int`Server unavailable. Retrying in ` + counter-- + Int` seconds, or reload.`, 'error');
-	} else {
-	  showMessage('');
-	  setupNetwork();
-	}
-      }, 1e3);
-      return;
-    }
-    const more = event.reason ? ' ' + event.reason : '';
-    showMessage(Int`The server connection has closed. Please reload.` + more, 'error');
-  };
-
-  connection.onmessage = event => { // Call the handler previously set using subscribe, if any.
-    const {key, data} = JSON.parse(event.data);
-    const handler = handlers[key];
-    if (!handler) return;
-
-    // If the publish was tagged for filtering by its publisher, check to see if
-    // the publisher was here.
-    if (data.messageTag) {
-      const index = inFlight.indexOf(data.messageTag);
-      if (index >= 0) {
-        inFlight.splice(index, 1);
-        return;
+      for (const key in handlers) { // If this is reconnecting, re-establish the subscriptions on the new socket.
+	subscribe(key, handlers[key]);
       }
-    }
+    };
 
-    handler(data, key);
-  };
-
-  for (const key in handlers) { // If this is reconnecting, re-establish the subscriptions on the new socket.
-    await subscribe(key, handlers[key]);
-  }
+    // onerror is of no help, as the event is generic.
+    connection.onclose = event => {
+      resolve(connectionPromise = null); // If anyone is waiting or will wait.
+      console.warn('websocket close', event.code, event.wasClean, event.reason);
+      if (event.reason === 'inactivity') return;
+      if (document.visibilityState === 'visible') { // Set up reconnect with countdown.
+	let counter = RETRY_SECONDS;
+	countdown = setInterval(() => {
+	  if (document.visibilityState !== 'visible') {
+	    console.log("Abandoning retry timeout for invisible tab.");
+	    clearInterval(countdown);
+	  } else if (counter > 1) {
+	    showMessage(Int`Server unavailable. Retrying in ` + counter-- + Int` seconds, or reload.`, 'error');
+	  } else {
+	    showMessage('');
+	    setupNetwork();
+	  }
+	}, 1e3);
+	return;
+      }
+      const more = event.reason ? ' ' + event.reason : '';
+      showMessage(Int`The server connection has closed. Please reload.` + more, 'error');
+    };
+  });
 }
+
 
 let inactivityTimer;
 export function resetInactivityTimer() { // Start a timer that will release the websocket after the given period.
@@ -88,16 +65,32 @@ export function resetInactivityTimer() { // Start a timer that will release the 
   setupNetwork();
   inactivityTimer = setTimeout(() => {
     showMessage(Int`Connection closed due to inactivity. Will reconnect on use.`, 'error');
-    connection.close(3000, 'inactivity');
+    close(3000, 'inactivity');
   }, INACTIVITY_SECONDS * 1e3);
 }
 
+
+// pub, sub, and receive
+const handlers = {}; // Mapping key => function(messageData) for all active subcriptions
+const renewals = {};
+export async function subscribe(key, handler) { // Assign handler for key, or remove any handler if falsy.
+  key = key.toString();
+  if (handler) {
+    handlers[key] = handler;
+    await send({method: 'subscribe', key});
+    renewals[key] = setTimeout(() => renewals[key] && subscribe(key, handler), 55 * 60e3);
+  } else {
+    delete handlers[key];
+    clearTimeout(renewals[key]);
+    delete renewals[key];
+    await send({method: 'unsubscribe', key});
+  }
+}
 const inFlight = [];
 export async function publish(key, data, timeToLive = 10 * 60e3) { // Publish data to subscribers of key.
-  await promise;
   key = key.toString();
 
-  // Iff this client has a handler for this key, evaluate it immediately and tag the
+  // IFF this client has a handler for this key, evaluate it immediately and tag the
   // publish with a recognizable value so that we can ignore its receipt.
   const publishData = { ...data };
   if (handlers[key]) {
@@ -108,22 +101,21 @@ export async function publish(key, data, timeToLive = 10 * 60e3) { // Publish da
     handlers[key](data, key);
   }
 
-  const message = {method: 'publish', key, data: publishData, timeToLive};
-  connection.send(JSON.stringify(message));
+  await send({method: 'publish', key, data: publishData, timeToLive});
 }
-const renewals = {};
-export async function subscribe(key, handler) { // Assign handler for key, or remove any handler if falsy.
-  await promise;
-  key = key.toString();
-  if (handler) {
-    handlers[key] = handler;
-    connection.send(JSON.stringify({method: 'subscribe', key}));
-    renewals[key] = setTimeout(() => renewals[key] && subscribe(key, handler), 55 * 60e3);
-  } else {
-    delete handlers[key];
-    clearTimeout(renewals[key]);
-    delete renewals[key];
-    connection.send(JSON.stringify({method: 'unsubscribe', key}));
-  }
-}
+function receive(message) {  // Call the handler previously set using subscribe, if any.
+  const {key, data} = message;
+  const handler = handlers[key];
+  if (!handler) return;
 
+  // If the publish was tagged for filtering by its publisher, check to see if the publisher was here.
+  if (data.messageTag) {
+    const index = inFlight.indexOf(data.messageTag);
+    if (index >= 0) {
+      inFlight.splice(index, 1);
+      return;
+    }
+  }
+
+  handler(data, key);
+};
