2eeebcb5eb1a049e5b5aa1a5e98fc97e4546948e
Howard Stearns <howard.stearns@gmail.com>
2026-04-12 13:59:03 -0700

    gratuitous version change

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const { Request, Response, URL, clients} = self;
-const serviceVersion = '0.0.60';
+const serviceVersion = '0.0.61';
 
 async function cacheFirst({request, event}) {
   // Handle request from any cache, else fetch and store it in serviceCache.
