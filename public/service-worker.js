ac89552850ff0539e814ea97222adec7c13fe722
Howard Stearns <howard.stearns@gmail.com>
2026-05-13 12:11:32 -0700

    versions

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const { Request, Response, URL, clients} = self;
-const serviceVersion = '0.0.75';
+const serviceVersion = '0.0.76';
 
 async function cacheFirst({request, event}) {
   // Handle request from any cache, else fetch and store it in serviceCache.
