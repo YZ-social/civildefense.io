6489ef817b3dc8270b4307d59de43ddca6042eef
Howard Stearns <howard.stearns@gmail.com>
2026-07-13 14:54:53 -0700

    handle inheritance of static dictionary

--- spec/conversationSpec.js
+++ spec/conversationSpec.js
@@ -27,5 +27,49 @@ describe("Conversation", function () {
     it("rejects changes by default.", function () {
       expect(() => Conversation.ensure({tag, label: 'other', agent})).toThrow();
     });
+    describe('caching', function () {
+      let keep = true;
+      let tag = "caching";
+      class CacheConversation extends Conversation {
+	update() { return keep && this; }
+	initialize() { return keep && this; }
+      }
+      it("happens if initialize answers conversation.", function () {
+	keep = true;
+	let initial = CacheConversation.ensure({tag, agent});
+	expect(initial).toBeTruthy();
+	expect(CacheConversation.ensure({tag, agent})).toBe(initial);
+	expect(CacheConversation.getConversation(tag)).toBe(initial);
+      });
+      it("is skipped if initialize answers falsy.", function () {
+	keep = false;
+	let initial = CacheConversation.ensure({tag, agent});
+	expect(initial).toBeFalsy();
+	expect(CacheConversation.getConversation(tag)).toBeFalsy();
+      });
+      it("keeps existing if update answers conversation.", function () {
+	keep = true;
+	let initial = CacheConversation.ensure({tag, agent});
+	expect(CacheConversation.ensure({tag, agent})).toBe(initial);
+	keep = true;
+	expect(CacheConversation.ensure({tag, agent})).toBe(initial);
+	expect(CacheConversation.getConversation(tag)).toBe(initial);
+      });
+      it("destroys existing if update answers falsy.", function () {
+	keep = true;
+	let initial = CacheConversation.ensure({tag, agent});
+	expect(CacheConversation.ensure({tag, agent})).toBe(initial);
+	keep = false;
+	expect(CacheConversation.ensure({tag, agent})).toBeFalsy();
+	expect(CacheConversation.getConversation(tag)).toBeFalsy();
+      });
+      it("ends with explicit destroy.", function () {
+	let initial = Conversation.ensure({tag, agent});
+	expect(initial).toBeTruthy();
+	expect(Conversation.getConversation(tag)).toBe(initial);
+	expect(initial.destroy()).toBeFalsy();
+	expect(Conversation.getConversation(tag)).toBeFalsy();
+      });
+    });
   });
 });
