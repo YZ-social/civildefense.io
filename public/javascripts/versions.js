bf5cbe5c351509848f5ac59d775683e8d5eba7ab
Howard Stearns <howard.stearns@gmail.com>
2026-08-03 15:20:55 -0700

    forgot change for previous hashtags

--- public/javascripts/versions.js
+++ public/javascripts/versions.js
@@ -3,10 +3,11 @@ export const appVersion = pkg.version; // Overall semver of app. Used in display
 export const dataVersion = globalThis.process?.env.EVENT_VERSION || appVersion.split('.')[0];  // Compatability differentiator used below.
 
 export function stripLeadingEmoji(string) { // Return string without any leading emoji (which might be of varying
-    // length) followed by an optional emoji break character and any whitespace.
-    // {Extended_Pictographic} is often recommended instead of {Emoji} as the latter includes numbers
-    // and symbols. However, the former misses, e.g., the flag emojis.
-    return string.replace(/^\p{Emoji}*\uFE0F?\s*/u, '') || string;
+  // length) followed by an optional emoji break character and any whitespace.
+  // {Extended_Pictographic} is often recommended instead of {Emoji} as the latter includes numbers
+  // and symbols. However, the former misses, e.g., the flag emojis.
+  return string.replace(/^\p{Emoji}*\uFE0F?\s*/u, '') ||
+    (string.endsWith(' ') ? '' : string); // All emoji counts, but if ending with space, it is the start of an emoji + \s + tag for which just the tag counts.
 };
 export function canonicalTag(tag) { // A string representing tag, without the (leading) emoji if any.
   return stripLeadingEmoji(tag).toLowerCase();
