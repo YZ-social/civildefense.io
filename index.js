7dc6169b0e6bfbf65ab24bb5b794576aab076e47
Howard Stearns <howard.stearns@gmail.com>
2026-08-03 17:41:37 -0700

    version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { P2PWebNetwork } from "./public/javascripts/p2pWebNetwork.js";
-export { agentTopic, alertTopic, canonicalTag } from "./public/javascripts/versions.js";
+export { topicRegion, agentTopic, alertTopic, canonicalTag } from "./public/javascripts/versions.js";
 export { getContainingCells } from "./public/javascripts/s2.js";
 export { location } from "./server/getLocation.js";
 export { deriveTopicIdBig } from "@axona/protocol";
