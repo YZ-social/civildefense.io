84f73f980fcc10bbf20e95c6721af342b8d6066c
Howard Stearns <howard.stearns@gmail.com>
2026-08-05 15:03:01 -0700

    fix nPortals

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const argv = yargs(hideBin(process.argv))
       .option('nPortals', {
 	alias: 'p',
 	type: 'number',
-	default: logicalCores - 3,
+	default: Math.max(2, logicalCores - 3),
 	description: "The number of steady nodes that handle initial connections."
       })
       .option('baseURL', {
