e13c383bb0219a9e54da89fe807f99f85671ab09
Howard Stearns <howard.stearns@gmail.com>
2026-08-13 12:37:18 -0700

    fix kill of missing pub

--- public/javascripts/pubsub.js
+++ public/javascripts/pubsub.js
@@ -116,10 +116,9 @@ export function unpublish(topic, msgId, {signWith}) {
   const topicId = deriveTopicId(topic);  
   cancel('pub', topicId, msgId);
   const envelope = removeBucket(data, 'pub', topicId, msgId);
-  if (envelope) {
-    envelope.deleted = true;
-    envelope.message = null;
-  }
+  if (!envelope) return {ok: false}; // we didn't have it.
+  envelope.deleted = true;
+  envelope.message = null;
   for (const [nodeTag, id] of getDataEntries('sub', topicId)) invoke(nodeTag, id, envelope);
   return {ok: true};
 }
