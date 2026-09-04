d4775bffb36be1cf95314417bac42783f8de811d
Howard Stearns <howard.stearns@gmail.com>
2026-09-04 09:14:41 -0700

    try not to confuse the LLM with irrelevant error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 const { Request, Response, URL, clients} = self;
 // Little point in trying to automatically pull this through package.json, as we need a byte change in THIS file to trigger a new worker.
-const serviceVersion = '4.5.28';
+const serviceVersion = '4.5.29';
 
 const cacheList = [ // The files we need.
   "/",
