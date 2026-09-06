cb53693b4acf97250b672992cb146ccff3498532
Howard Stearns <howard.stearns@gmail.com>
2026-09-06 10:23:09 -0700

    oops

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,7 +7,7 @@ import { P2PWebNetwork } from './p2pWebNetwork.js';
 import { getPointInCell } from './s2.js';
 import { Alert, getShareableURL, share } from './alert.js';
 import { map, showMessage, updateLocation, recenterMap } from './map.js';
-//import './service-manager.js'; // Comment this out and kill service-workers for reload-to-get-latest behavior during development.
+import './service-manager.js'; // Comment this out and kill service-workers for reload-to-get-latest behavior during development.
 window.P2PWebNetwork = P2PWebNetwork;
 
 document.getElementById('appVersion').textContent = appVersion;
